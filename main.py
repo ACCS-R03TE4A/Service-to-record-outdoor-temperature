f348fda1a171fa70c06e7005a4506d70db0b411c
te180453 <te180453jc21@gmail.com>
2021-12-22 12:22:08 +0900

    update

--- main.py
+++ main.py
@@ -5,22 +5,28 @@ import db
 from search_temperature import get_temperature
 from setting import Setting
 import sys
+config = {}
 try:
+    print("Load config.json")
     with open("config.json","r") as f:
         config = json.load(f)
 except FileNotFoundError:
+    print("Could not find config.json. Create it.")
     with open("config.json","w") as f:
-        json.dump({"recording_interval(min)":1})
+        json.dump({"recording_interval(min)":1}, f)
 
 #郵便番号をデータベースからとってくる
 if Setting.objects.all().count() == 0:
     print(Setting(postnumber="980-0013").save())
-pn = Setting.objects.first().postnumber
 
 while True:
     try:
+        pn = Setting.objects.first().postnumber
+        print(f"Post number : {pn}")
         temp = get_temperature(pn)
-        requests.get(f"http://localhost:5000/temperatureActual?sNumber=2&tActual={temp}")
+        print(f"Temperature : {temp}")
+        ret = requests.get(f"http://localhost:5000/temperatureActual?sNumber=2&tActual={temp}")
+        print(f"Transmission success! Response messege : {ret.text}")
     except Exception as error:
         print(error, file=sys.stderr)
     time.sleep(config["recording_interval(min)"] * 60)
